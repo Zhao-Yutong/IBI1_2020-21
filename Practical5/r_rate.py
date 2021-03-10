4917e5451c3127cea352ce804711c2fa11bda73e
Zhao-Yutong <79929472+Zhao-Yutong@users.noreply.github.com>
2021-03-10 16:52:05 +0800

    Update r_rate.py

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,5 +1,9 @@
+#Define the variable n as the origin infected individuals
 n = 84
+#Assume the given r rate is 1.2
 r = 1.2
+#Repeat for 5 times as we need to find out the number of infected individuals after 5 rounds of infection
 for i in  range(1,6):
+#Every new round is the old round multiplies the r rate
     n = n *r
     print "The number of infected individuals is", n
