e74538c0a79d1ea6a72b86599ba8d2fe5fc272c4
Zhao-Yutong <79929472+Zhao-Yutong@users.noreply.github.com>
2021-03-10 16:40:57 +0800

    Update collatz.py

--- Practical5/collatz.py
+++ Practical5/collatz.py
@@ -1,6 +1,9 @@
+#Value the viariables as the first and second terms of the sequence
 x = 1
 y = 1
+#Repeat
 for i in range(3,14):
+#Value the adjacent three items in the sequence  
     z = x + y
     x = y
     y = z
