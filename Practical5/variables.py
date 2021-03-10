5d8524c72ef8344cc1d32953692cefe7a7a298a3
Zhao-Yutong <79929472+Zhao-Yutong@users.noreply.github.com>
2021-03-10 16:29:47 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,3 +1,4 @@
+#Assign a value to a value
 a = 110302
 b = 190784
 c = 100321
@@ -5,10 +6,12 @@ d = abs(a-c)
 e = abs(a-b)
 print "d =",d
 print "e =",e
+#To judge which value is greater
 if d > e:
    print "d is greater"
 else:
    print "e is greater"
+#To determine the truth or falsehood of a proposition
 X = True
 Y = False
 Z = (X and not Y)or(Y and not X)
